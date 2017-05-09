15cf0e0950951ae72ba03af2ffc2059484f0b99d
RemcoG-immalle <remco.goyvaerts@immalle.be>
2017-05-09 15:38:01 +0200

    Made models

---
+++ database/migrations/2017_05_09_133100_create_walls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('walls', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('wall_name');
+            $table->integer('user_id');
+            $table->string('theme')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('walls');
+    }
+}
