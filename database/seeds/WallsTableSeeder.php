aeb4ef731f2c8576201c8b3f175ae1e39bd4168e
RemcoG-immalle <remco.goyvaerts@immalle.be>
2017-05-09 15:46:44 +0200

    Added seeders

---
+++ database/seeds/WallsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class WallsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
