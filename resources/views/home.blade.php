2a64956814e732881f074f720a7fd3259a352689
RemcoG-immalle <remco.goyvaerts@immalle.be>
2017-05-11 15:38:22 +0200

    Search bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
 
   <!-- Materialize -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
 
   <!-- Google Font -->
@@ -16,7 +17,19 @@
 </head>
 
 <body>
-  <div id="top-border"></div>
+
+  <!-- Search Bar-->
+  <nav class="search">
+  <div class="nav-wrapper">
+    <form>
+      <div class="input-field">
+        <input id="search" type="search" required>
+        <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+        <i class="material-icons">close</i>
+      </div>
+    </form>
+  </div>
+  </nav>
 
   <!-- Ribbon -->
   <div class="center-wrap">
