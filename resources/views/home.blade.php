43cf26e15ee3fc87132e36620f79ccc5731c373c
RemcoG-immalle <remco.goyvaerts@immalle.be>
2017-05-11 11:03:33 +0200

    Home page with login bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<html>
+<head>
+  <title>Post IT</title>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+  <!-- Materialize -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+
+  <!-- Google Font -->
+  <link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" href="{{ URL::asset('css/style_home.css') }}">
+</head>
+
+<body>
+  <div id="top-border"></div>
+
+  <!-- Ribbon -->
+  <div class="center-wrap">
+    <div class="title-container">
+      <div class="ribbon-left"></div>
+      <div class="backflag-left"></div>
+      <div class="title"><a href="/">Post IT</a></div>
+      <div class="backflag-right"></div>
+      <div class="ribbon-right"></div>
     </div>
-</div>
+  </div>
+
+  <div class="container">
+
+    <!-- Content -->
+    <p class="center-align">Dit wordt een webapplicatie waarbij je op verschillende Walls (per thema) een post-it kan achterlaten met een korte boodschap.</p>
+
+  </div>
+
+  <div id="bottom-border"></div>
+</body>
+</html>
+
 @endsection
