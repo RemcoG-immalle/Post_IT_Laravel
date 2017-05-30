d1883edaa7c55d169bce06e63f8f158295bc7d54
RemcoG-immalle <remco.goyvaerts@immalle.be>
2017-05-30 08:52:21 +0200

    Changed wall seeds

--- database/seeds/WallsTableSeeder.php
+++ database/seeds/WallsTableSeeder.php
@@ -12,12 +12,12 @@ class WallsTableSeeder extends Seeder
     public function run()
     {
       DB::table('walls')->insert([
-        'wall_name' => str_random(10),
+        'wall_name' => "Sport",
         'user_id' => 1,
       ]);
 
       DB::table('walls')->insert([
-        'wall_name' => str_random(10),
+        'wall_name' => "Gaming",
         'user_id' => 2,
       ]);
     }
