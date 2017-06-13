1f509f2c9068dbe9b843cdcd22baf8b0e91f4e65
RemcoG-immalle <remco.goyvaerts@immalle.be>
2017-06-13 16:35:39 +0200

    Temp 404 made

---
+++ resources/views/404.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<html>
+<head>
+  <title>Post IT</title>
+
+  <!-- Materialize -->
+  <link rel="stylesheet" href="{{ URL::asset('css/materialize.css') }}">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script src="{{ URL::asset('js/materialize.js') }}"></script>
+
+  <!-- Google Font -->
+  <link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" href="{{ URL::asset('css/style_home.css') }}">
+</head>
+
+<body>
+  <div class="container">
+
+    <!-- Title -->
+    <h1 class="center-align">Looks like this wall doesn't exist. If you want a wall with this theme, try and contact the admin.</h1>
+
+  </div>
+
+  <div id="bottom-border"></div>
+  <script src="{{ URL::asset('js/script_home.js') }}"></script>
+</body>
+</html>
+@endsection
